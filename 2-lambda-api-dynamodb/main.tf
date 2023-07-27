71ee2fbb1f8723e833c0372f16e90e229f187a3e
Pedro Rodrigues <pedrovitorlima@gmail.com>
2023-07-27 16:33:05 +1000

    change terraform

--- 2-lambda-api-dynamodb/main.tf
+++ 2-lambda-api-dynamodb/main.tf
@@ -7,3 +7,13 @@ terraform {
     dynamodb_table = "terraform-state-locking"
   }
 }
+
+resource "aws_dynamodb_table" "names" {
+  name = "names"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "id"
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
